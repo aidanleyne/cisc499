65456f6ba54768377ad0d33c1944bdc1d2d7cc80
aidanleyne <aidan@leyne.com>
2024-01-25 15:56:01 -0500

    Browser test page

---
+++ Profile.py
@@ -0,0 +1,11 @@
+class Profile:
+    def __init__(self, vector, id):
+        self.vector = vector
+        self.id = id
+    
+
+def main():
+    print("[id, vector] : [%s, %s]", self.id, self.vector)
+
+if __name__ == "__main__":
+    main()
