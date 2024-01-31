b4787917b4cfdc5c3c0d47a159a6569b15cd8e83
aidanleyne <aidan@leyne.com>
2024-01-30 19:43:55 -0500

    setup logging and added comments

--- load_files.py
+++ load_files.py
@@ -1,16 +1,29 @@
 import os
+import logging
 import csv
 import pandas as pd
 
+###LOGGING SETUP###
+logging.basicConfig(filename="dataLoad.log",
+                    format='%(asctime)s : %(name)s : %(message)s',
+                    filemode='a',
+                    level=logging.DEBUG)
+logger = logging.getLogger()
+
+###PATH TO DATA####
 PATH = 'data'
 
+"""
+Gets and returns all specified csv filenames from a directory
+Directory is set by global PATH variable
+No Arguments 
+"""
 def get_files():
     files = os.listdir(PATH)
 
     for file in files:
         if '.csv' not in file:
             files.remove(file)
-
     return files
 
 def get_headers(filename):
@@ -21,7 +34,6 @@ def get_headers(filename):
             headers.append(row[0])
         
     headers.pop(0)
-    print(headers)
 
     return headers
 
@@ -29,11 +41,17 @@ def get_data(filename, headers):
     return pd.read_csv(str(PATH + '/' + filename), names=headers, encoding='latin1', quotechar='"')
 
 def main():
+    logger.info(" ====START OF LOG====")
+
     files = get_files()
+    logger.debug("FILENAMES :" +  str(files))
 
     for i in range(0, len(files), 2):
-        print(files[i])
-        get_data(files[i], get_headers(files[i+1]))
+        headers = get_headers(files[i+1])
+        logger.debug("FOR : %s, HEADERS : %s", files[i], str(headers))
+        #get_data(files[i], headers)
+
+    logger.info("====END OF LOG==== \n")
 
 if __name__ == "__main__":
     main()
