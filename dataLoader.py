7b8816e4b73f1fdd0ee388e7c5c290768585f31d
aidanleyne <aidan@leyne.com>
2024-02-09 17:51:27 -0500

    low memeory flag only skipped all files (#12)

--- dataLoader.py
+++ dataLoader.py
@@ -165,8 +165,7 @@ class TSVReader:
     """
     def load(self, filename):
         try:
-            return pd.read_csv(str(self._PATH + '/' + filename), header=0, delimiter='\t', 
-                               low_memeory=False, dtype={
+            return pd.read_csv(str(self._PATH + '/' + filename), header=0, delimiter='\t', dtype={
                 'PARTICIPANT_ID': 'int32',
                 'TEST_SECTION_ID': 'int32',
                 'SENTENCE': 'string',
