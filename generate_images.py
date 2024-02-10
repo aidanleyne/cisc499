192d10a27f4e7cba898aa8bb7e1b84d533f21a01
aidanleyne <aidan@leyne.com>
2024-02-09 19:05:52 -0500

    adding some logging for image generation

--- generate_images.py
+++ generate_images.py
@@ -2,6 +2,7 @@
 Generates images for all desktop data files in the /data folder.
 """
 import sys
+from tqdm import tqdm as tq
 from ImageGenerator import ImageGenerator
 from dataLoader import TSVReader
 
@@ -18,10 +19,12 @@ elif len(sys.argv) > 2:
     INPATH = sys.argv[1]
     OUTPATH = sys.argv[2]
 
+print("*** Loading Files... ***")
 reader = TSVReader(INPATH)
 gen = ImageGenerator(OUTPATH)
 
-for df in reader.data:
-    gen.generate_image(reader.data[df], df)
+print("*** Loading Files... ***")
+for filename, df in tq(reader.data.items()):
+    gen.generate_image(filename, df)
 
 gen.close()
