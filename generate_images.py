af72c9cb85a64d29b06278855a7b3e0931ba899e
aidanleyne <aidan@leyne.com>
2024-03-08 18:55:25 -0500

    .

--- generate_images.py
+++ generate_images.py
@@ -83,6 +83,7 @@ gen = ImageGenerator(OUTPATH)
 
 print("\n*** Creating Images... ***")
 pool = multiprocessing.Pool(processes=NUM_OF_CORES)
-for _ in tq(pool.map(run, reader.data.items()), total=len(reader.data.items()))
+for _ in tq(pool.map(run, reader.data.items()), total=len(reader.data.items())):
+    pass
 
 gen.close()
